6b0560f4bd8f53057a1bbb952da0f583550c4085
shreeshk <71731273+shreeshk@users.noreply.github.com>
2022-01-30 17:59:13 -0500

    Update Autonomous.java

--- src/main/java/frc/robot/commands/autonomous/Autonomous.java
+++ src/main/java/frc/robot/commands/autonomous/Autonomous.java
@@ -16,11 +16,13 @@ public class Autonomous {
           Rotation2d.fromDegrees(0));
       var crossScale = new Pose2d(Units.feetToMeters(5), Units.feetToMeters(5),
           Rotation2d.fromDegrees(0));
-  
+  //these are the initial and final locations in (x,y) format as well as the initial and final headings in degrees)
+     
       ArrayList <Translation2d> interiorWaypoints = new ArrayList <Translation2d>();
       interiorWaypoints.add(new Translation2d(Units.feetToMeters(3), Units.feetToMeters(2)));
       interiorWaypoints.add(new Translation2d(Units.feetToMeters(1), Units.feetToMeters(3)));
-  
+  //these are the points that the robot must pass over on its way from the initial point to the end point (arbitrary points are in place for now)
+     
       TrajectoryConfig config = new TrajectoryConfig(Units.feetToMeters(12), Units.feetToMeters(12));
       config.setReversed(false);
   
