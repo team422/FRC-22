df5e9ae32cb8bdbade2f6afa9307b45f5dbd1df5
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-02-19 12:01:57 -0500

    recommitted

---
+++ src/main/java/frc/robot/subsystems/drivebases/DriveBase.java
@@ -0,0 +1,96 @@
+package frc.robot.subsystems.drivebases;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * Drivebase interface for all drivebases
+ */
+public abstract class DriveBase extends SubsystemBase {
+
+    public MotorControllerGroup leftSide;
+    public MotorControllerGroup rightSide;
+    public DifferentialDrive driveBase;
+
+    public double leftMotorTicks = 0;
+    public double rightMotorTicks = 0;
+
+    public DriveBase() {
+        setSubsystem("DriveBase");
+
+        this.driveBase = new DifferentialDrive(leftSide, rightSide);
+    }
+
+    /**
+     * Sets drive train motors.
+     * @param left Left side motors' velocity (-1 to 1)
+     * @param right Right side motors' velocity (-1 to 1)
+     */
+    public void setMotors(double left, double right) {
+        leftSide.set(left);
+        rightSide.set(right);
+    }
+
+    /**
+     * Sets the left side drive train motors
+     * @param left Left side motors' velocity (-1 to 1)
+     */
+    public void setLeftMotors(double left) {
+        leftSide.set(left);
+    }
+
+    /**
+     * Sets the right side drive train motors
+     * @param right Right side motors' velocity (-1 to 1)
+     */
+    public void setRightMotors(double right) {
+        rightSide.set(right);
+    }
+
+    /**
+     * Sets drive train motors to zero, effectively stopping the bot.
+     */
+    public void stopMotors() {
+        leftSide.stopMotor();
+        rightSide.stopMotor(); //better apparently according to Yash (stopMotor instead of set(0))
+    }
+
+    public void curvatureDrive(double xSpeed, double zRotation, boolean allowTurnInPlace){
+        driveBase.curvatureDrive(xSpeed, zRotation, allowTurnInPlace);
+    }
+
+    public void tankDrive(double leftSpeed, double rightSpeed, boolean squareInputs){
+        driveBase.tankDrive(leftSpeed, rightSpeed, squareInputs);
+    }
+
+    public void arcadeDrive(double xSpeed, double zRotation, boolean squareInputs){
+        driveBase.arcadeDrive(xSpeed, zRotation, squareInputs);
+    }
+
+    /**
+     * @return Left side position in ticks. If statement exists because Falcon uses a different motor as leader than 
+     * the other drivebases 
+     */
+    public abstract double getLeftPosition();
+
+    /**
+     * @return Right side position in ticks.
+     */
+    public abstract double getRightPosition();
+
+    /**
+     * @return Angle at which the robot is positioned in degrees
+     */
+    public abstract double getGyroAngle();
+
+    /**
+     * Resets the reference point used to calculate distance traveled. Does not physically change the encoder value.
+     */
+    public abstract void zeroEncoderPosition();
+
+    /**
+     * Sets the gyro angle to zero.
+     */
+    public abstract void zeroGyroAngle();
+}
---
+++ src/main/java/frc/robot/subsystems/shooters/BaseShooter.java
@@ -0,0 +1,77 @@
+package frc.robot.subsystems.shooters;
+
+import frc.robot.RobotMap;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public abstract class BaseShooter extends SubsystemBase{
+
+    // For BangBangController, need velocity of current motor and the desired velocity
+    // https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/math/controller/BangBangController.html#getSetpoint()
+    // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/controllers/bang-bang.html
+    
+    public BaseShooter(){
+        setSubsystem("FlyBoi");
+    }
+
+
+    /**
+    * Function to set the shoot speed (-1, 1)
+    * @param mainSpeed speed to set left and right roller (-1, 1)
+    * @param topSpeed speed for the top (-1, 1)
+    */
+    public void setShootSpeeds(double... motorSpeeds){
+    }
+    
+    /**
+    * Function to stop the shooter.
+    */
+    public void stopShoot(){
+    }
+    
+    /**
+    * Function to get speed of left Motor (-1, 1).
+    * @return returns Speed of of left motor (-1, 1).
+    */
+    public abstract double getLeft();
+
+    /**
+    * Function To get spEed of right motor (-1, 1).
+    * @return returns speed of Right motor (-1, 1).
+    */
+    public abstract double getRight();
+    
+    /**
+    * Function to get speed of top motor (-1, 1).
+    * @return returns speed Of Top motor (-1, 1).
+    */
+    public abstract double getTop();
+
+    /**
+    * Function to get velocity Of left motor (raw sensor units/ticks).
+    * @return Returns velocity Of left motor (raw Sensor units/ticks).
+    */
+    public abstract double getLeftVelocity();
+
+    /**
+     * Function to Get velocity of Right motor (raw Sensor units/ticks).
+     * @return returns velocity of right Motor (raw sensor units/Ticks).
+     */
+    public abstract double getRightVelocity();
+    
+    /**
+    * function to get Velocity of top motor (Raw sensor units/ticks).
+    * @return Returns Velocity Of Top Motor (Raw Sensor Units/Ticks).
+    */
+    public abstract double getTopVelocity();
+
+    public void voltageShootiShoot(double... setVelocities) {
+    }
+
+    public void speedShootiShoot(double... setpoints) {
+    }
+}
